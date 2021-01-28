6d99b77b8fb522488ae5c4a19e53c87f150f541f
PraveenJaya <40675174+PraveenJaya@users.noreply.github.com>
2021-01-28 21:05:10 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,14 +1,23 @@
-typedef struct Stats{
-	  float min;
-	  float max;
-	  float average; 
-	}stats_t;
-	
-	stats_t compute_statistics(float* numset, int setlen);
-	
-	typedef void (*alerter_funcptr)();
-	void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-	
-	extern int emailAlertCallCount;
-	extern int ledAlertCallCount;
+#include <stdlib.h>
+#include "alerters.h"
+
+/* Structure for Statistics */
+typedef struct Statistics{
+  float min;
+  float max;
+  float average; 
+}stats_s;
+
+/* MACROs and Variables */
+#define TRUE 1
+#define FALSE 0
+
+/* Public decalarations */
+stats_s compute_statistics(float* numberset, int setlength);
+typedef void (*alerter_funcptr)();
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], stats_s computedStats);
+
+/* Extern variables */
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
 
