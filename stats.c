1307f1e89c0edf1386f885fda3aefcfe7472dbc8
PraveenJaya <40675174+PraveenJaya@users.noreply.github.com>
2021-01-28 19:23:22 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,15 +2,15 @@
 	
 	stats_t compute_statistics(float* numset, int setlen)
 	{
-	    int i, j;
+	    int itr, j;
 	    float key = 0, sum = 0;
 	    stats_t statData;
 	    
 	    /* Sorting the given number */
-	    for (i = 1; i < setlen; i++) 
+	    for (itr = 1; itr < setlen; itr++) 
 	    { 
-	        key = numset[i]; 
-	        j = i - 1; 
+	        key = numset[itr]; 
+	        j = itr - 1; 
 	        while (j >= 0 && numset[j] > key) { 
 	            numset[j + 1] = numset[j]; 
 	            j = j - 1; 
@@ -18,8 +18,8 @@
 	        numset[j + 1] = key; 
 	    }
         
-        /* Average of numbers */
-	    for(i=0;i<setlen;i++)
+            /* Calculate Average of numbers */
+	    for(itr=0;itr<setlen;itr++)
 	    {
 	        sum += numset[i];
 	    }
