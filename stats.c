dcc59b52c079ba2a0d333d81b6dedb659d5d36cb
PraveenJaya <40675174+PraveenJaya@users.noreply.github.com>
2021-01-28 19:11:36 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,32 @@
 #include "stats.h"
+	
+	stats_t compute_statistics(float* numset, int setlen)
+	{
+	    int i, j;
+	    float key = 0, sum = 0;
+	    stats_t statData;
+	    
+	    /* Sorting the given number */
+	    for (i = 1; i < setlength; i++) 
+	    { 
+	        key = numset[i]; 
+	        j = i - 1; 
+	        while (j >= 0 && numset[j] > key) { 
+	            numset[j + 1] = numset[j]; 
+	            j = j - 1; 
+	        } 
+	        numset[j + 1] = key; 
+	    }
+        
+        /* Average of numbers */
+	    for(i=0;i<setlength;i++)
+	    {
+	        sum += numset[i];
+	    }
+	    statData.average = sum/setlength;
+	    statData.min = numset[0];
+	    statData.max = numset[setlength-1]; 
+	
+	    return statData;
+	}
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
