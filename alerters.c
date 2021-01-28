c0e5dce3bcece26c171da6e79d23e159488531f1
PraveenJaya <40675174+PraveenJaya@users.noreply.github.com>
2021-01-28 21:19:14 +0530

    Update alerters.c

--- alerters.c
+++ alerters.c
@@ -1,10 +1,21 @@
+/* Module includes */
 #include "stats.h"
 
-void emailAlerter()
+/* Function name: emailAlerter
+   Function Brief- This API will be called to notify threshold > max
+   Paramaters - void
+   Return Type - void
+*/
+void emailAlerter(void)
 {
     emailAlertCallCount = 1;
 }
 
+/* Function name: ledAlerter
+   Function Brief- This API will be called to notify threshold > max
+   Paramaters - void
+   Return Type - void
+*/
 void ledAlerter()
 {
      ledAlertCallCount = 1;
